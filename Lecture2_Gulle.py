35d624800a981e4b8ef82bd4b636f437432a8384
Jo Elise G. Gulle <jggulle1@up.edu.ph>
2024-02-27 16:12:39 +0800

    lecture notes

---
+++ Lecture2_Gulle.py
@@ -0,0 +1,35 @@
+print("Kumusta Mundo")
+print(type("Kumusta Mundo"))
+print(type(str(1)))
+# hello hello
+# WHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+print("i love " + "djungie")
+
+"""
+this is a multi line comment!
+i love djungelskog
+i love my family 
+i wanna eat a donut rn
+"""
+print(type("string"))
+print(type(7))
+print(type(2.83))
+print(type(1+3j))
+
+x = 7
+y = 19
+
+print("sum of x and y:",x+y)
+print("hehe funny number go brrrrr", x-y)
+print("hehe funny number go brrrrr", x*y)
+print("hehe funny number go brrrrr", x/y)
+print("hehe funny number go brrrrr", x//y)
+print("hehe funny number go brrrrr", x**y)
+print("hehe funny number go brrrrr", x%y)
+print("hehe funny number go brrrrr", abs(y))
+print("hehe funny number go brrrrr", float(y))
+print("hehe funny number go brrrrr", complex(y))
+print("hehe funny number go brrrrr", divmod(y,x))
+
+grocery = ["eggs", "milk", "bread", "sugar"]
+print (grocery[3])
