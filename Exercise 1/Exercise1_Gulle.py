96670baeef70bc009e789c03745358cbae0e1b94
Jo Elise G. Gulle <jggulle1@up.edu.ph>
2024-03-14 12:56:55 +0800

    edits

--- Exercise 1/Exercise1_Gulle.py
+++ Exercise 1/Exercise1_Gulle.py
@@ -1,11 +1,13 @@
 """
-GE 120: A cool programming subject
-The first time I've ever tried to code!
+Exercise 1
+Gulle, Jo Elise G.
+2023-06414
 """
 
 # Converting DMS to DD
 
 print("----- DMS to DD -----")
+
 dms = "267-58-99.99" #input your dms value here!
 print("DMS value:", dms)
 values = dms.split("-")
@@ -20,7 +22,8 @@ print("DD value:", round(dd,6))
 # Converting DD to DMS
 
 print("----- DD to DMS -----")
-DD = "359.98298347"
+
+DD = "359.98298347" #input your dd value here!
 print("DD value:",DD)
 
 DEGREES = int(float(DD))
@@ -35,3 +38,13 @@ print("*minutes_int:",MINUTES_INT)
 SECONDS = float((MINUTES_INT - MINUTES)*60)
 print("*seconds:",round(SECONDS,2))
 print("DMS value:",DEGREES, MINUTES_INT, round(SECONDS,2))
+
+"""
+notes to self:
+DD, DMS, MINUTES, SECONDS, etc. variables were made in both lowercase and capital versions to avoid referencing conflicts.
+Is there a way to circumvent this? I heard about global variables, so there are probably local ones.
+I'll look into that.
+
+I have to be mindful of the variable types next time, such as string, float, etc.
+I need more practice.
+"""
---
+++ Exercise 2/Exercise2_Gulle.py
@@ -0,0 +1,68 @@
+"""
+Exercise 2
+Gulle, Jo Elise G.
+2023-06414
+"""
+counter = 1
+lines = []
+
+print("----------LINE TABULATION----------")
+
+while True:
+    print()
+    print("Line No.", counter)
+
+    try:
+        distance = float(input("Distance: "))
+    except ValueError:
+        print("\n Invalid input. Please input numbers only.\n")
+        continue
+
+
+    azimuth = input("Azimuth (S): ")
+
+
+    if "-" in azimuth:
+        degrees, minutes, seconds = azimuth.split("-")
+        azimuth = ((int(degrees))+(int(minutes)/60)+(float(seconds)/3600))
+    else:
+        azimuth = float(azimuth)%360
+
+    if azimuth > 0 and azimuth < 90:
+        bearing = 'S {: ^5} W'.format(azimuth)
+    elif azimuth > 90 and azimuth < 180:
+        bearing = 'N {: ^5} W'.format(180 - azimuth)
+    elif azimuth > 180 and azimuth <270:
+        bearing = 'N {: ^5} E'.format(azimuth - 180)
+    elif azimuth >270 and azimuth <360:
+        bearing = 'S {: ^5} E'.format(360 - azimuth)
+    elif azimuth == 0:
+        bearing = "Due South"
+    elif azimuth == 90:
+        bearing = "Due West"
+    elif azimuth == 180:
+        bearing = "Due North"
+    elif azimuth == 270:
+        bearing = "Due East"
+    else:
+        bearing = "???"
+
+    line = (counter, distance, bearing)
+    lines.append(line)
+
+    yesno = input("Add new line? ")
+    if yesno.lower() == "Yes" or yesno.lower() == "y":
+        counter = counter + 1
+        continue
+    else:
+        break
+
+
+print("\n\n")
+print('{: ^10} {: ^10} {: ^10}'.format("LINE", "DISTANCE","BEARING"))
+
+for line in lines:
+    print('{: ^10} {: ^10} {: ^10}'.format(line[0], line[1], line[2]))
+
+print("\n\n")
+print("----------END----------")
